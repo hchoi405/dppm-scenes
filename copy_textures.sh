3fe562b42ae8b3786489a44fc2f80ecf081111bb
Hajin Choi <hchoi405@gmail.com>
2021-01-18 11:52:14 +0900

    Add misc.

---
+++ copy_textures.sh
@@ -0,0 +1,3 @@
+find ./scenes/ -iname '*.jpg' -exec ./saveCopy {} ./textures \;
+find ./scenes/ -iname '*.png' -exec ./saveCopy {} ./textures \;
+find ./scenes/ -iname '*.bmp' -exec ./saveCopy {} ./textures \;
